4401b9b129b25c62487299f5b84ad6b93db6d502
kokilasenthilvelan <102371475+kokilasenthilvelan@users.noreply.github.com>
2024-06-22 22:52:03 +0530

    Add files via upload
    Added assignment for Day1 of Week1

---
+++ day1/Firefox.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class Firefox {
+
+	public static <string> void main(String[] args) {
+		float browserVersion = 100.2f;
+		String browserName = "Firefox";
+		boolean isVisible = true;
+		int releaseYear = 1998;
+		char browserLogo = 'f';
+		
+		System.out.println("BrowserVersion is "+browserVersion);
+		System.out.println("BrowserName is "+browserName);
+		System.out.println("BrowserVisbility is "+isVisible);
+		System.out.println("Release year is "+releaseYear);
+		System.out.println("Browser Logo is "+browserLogo);
+		
+
+	}
+
+}
---
+++ day1/Welcome.java
@@ -0,0 +1,10 @@
+package week1.day1;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Java Maven");
+
+	}
+
+}
